766dac3dc2085ff67a202c878287403e1c6c0090
xp <xp@CETYSDAM028>
2018-03-05 19:46:43 +0100

    ya lo chequea

---
+++ src/sudoku/Sudoku.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author xp
+ */
+public class Sudoku {
+    public boolean chequeaSudoku (int[][]matriz){
+        //
+        int error = 0;
+        if (matriz.length == 0){
+            return false;
+        }
+        for (int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[i].length;j++){
+                error -= 1;
+                for (int k=0;k<matriz.length;k++){
+                    for(int m=0;m<matriz[k].length;m++){
+                        if(matriz[i][j] == matriz[k][m]){
+                            error+=1;
+                        }
+                    }
+                }
+            }
+        }     
+        if (error == 0){
+            return true;
+            
+        }
+        
+             return false;
+    }
+
+    
+    
+    
+    public static void main(String[] args) {
+        Sudoku pruebaSudoku = new Sudoku();
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{},{4,5,6},{7,8,9}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{3,3,3},{4,5,6},{7,8,9}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1},{},{}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{9,9,9}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
+        
+        
+    }
+    
+}
---
+++ src/sudoku/ernes.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author xp
+ */
+public class ernes {
+    public boolean chequeaSudoku (int[][]matriz){
+        //
+        int error = 0;
+       
+        for (int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz[i].length;j++){
+                error -=1;
+            
+                for (int k=0;k<matriz.length;k++){
+                    for(int m=0;m<matriz[k].length;m++){
+                        if(matriz[i][j] == matriz[k][m]){
+                            error+=1;
+                            System.out.println(error);
+                        }
+                    }
+                }
+            }
+            
+        }     
+        if (error == 0){
+            return true;
+            
+        }
+        
+             return false;
+    }
+
+    
+    
+    
+    public static void main(String[] args) {
+        ernes pruebaSudoku = new ernes();
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{},{4,5,6},{7,8,9}}));
+        
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{3,3,3},{4,5,6},{7,8,9}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{},{},{}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{9,9,9}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{9,8,9}}));
+        
+        
+    }
+    
+}
