c0ee8b8165e0ecea397a1f6fb1c822962bf8ea60
xp <xp@CETYSDAM028>
2018-03-05 19:50:32 +0100

    ya esta el sudoku

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -14,13 +14,13 @@ import java.util.Arrays;
 public class Sudoku {
     public boolean chequeaSudoku (int[][]matriz){
         //
-        int error = 0;
+        int error = -9;
         if (matriz.length == 0){
             return false;
         }
         for (int i=0;i<matriz.length;i++){
             for(int j=0;j<matriz[i].length;j++){
-                error -= 1;
+                
                 for (int k=0;k<matriz.length;k++){
                     for(int m=0;m<matriz[k].length;m++){
                         if(matriz[i][j] == matriz[k][m]){
@@ -45,7 +45,7 @@ public class Sudoku {
         Sudoku pruebaSudoku = new Sudoku();
         System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{},{4,5,6},{7,8,9}}));
         System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{3,3,3},{4,5,6},{7,8,9}}));
-        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1},{},{}}));
+        System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{},{},{}}));
         System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{9,9,9}}));
         System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
         System.out.println(pruebaSudoku.chequeaSudoku(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
